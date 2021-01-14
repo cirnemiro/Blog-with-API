570ac1bdd38a3a5551d5160713aa17c04e1199ae
wsierra010 <wsierra010@gmail.com>
2021-01-14 15:23:07 +0100

    Sincronize users name and create Modal Window

--- scripts.js
+++ scripts.js
@@ -26,48 +26,87 @@ $(document).ready(function () {
     
     const printPosts = ()=>{
         // imprimirremos los posts
-        posts.forEach(e=>{
-            // variables DOM
-            const $posts = $('.posts');
-            // crear elemento html
-            const divElement = $('<div></div>');
-            const divElementData = $('<div></div>');
-            const divElementicons = $('<div></div>');
-            // ponerle el contenido al elemento HTML
-            divElementData.html(`
-                <h3 class="post_element_data__title">${e.title}</h3>
-                <p class="post_element_data__body">${e.body}</p>
-            `);
-            divElementicons.html(`
-                <!-- svg edit -->
-                <i class="fa fa-pencil-square-o editIcon" aria-hidden="true"></i>
-                <!-- svg delete -->
-                <i class="fa fa-times-circle-o editIcon" aria-hidden="true"></i>
-            `)
-            // ponerle clases al elemento
-            divElement.addClass('post__element');
-            divElementData.addClass('post_element__data');
-            divElementicons.addClass('post_element__icons');
-            // añadir dataset
-            // divElement.dataset.url = e.id
-            divElementData.attr('data-id', e.id);
-            // append del elemento
-            // $('.post__element').attr('data-id', e.id);
-            divElement.append(divElementData);
-            divElement.append(divElementicons);
-            $posts.append(divElement);
-            // add event listener
-            
-            divElementData.click(onClickData);
+        posts.forEach((e,i)=>{
+            if (i<=10) {
+                // variables DOM
+                const $posts = $('.posts');
+                // crear elemento html
+                const divElement = $('<div></div>');
+                const divElementData = $('<div></div>');
+                const divElementicons = $('<div></div>');
+                // ponerle el contenido al elemento HTML
+                divElementData.html(`
+                    <h3 class="post_element_data__title">${e.title}</h3>
+                    <p class="post_element_data__body">${e.body}</p>
+                `);
+                divElementicons.html(`
+                    <!-- svg edit -->
+                    <i class="fa fa-pencil-square-o editIcon" aria-hidden="true"></i>
+                    <!-- svg delete -->
+                    <i class="fa fa-times-circle-o editIcon" aria-hidden="true"></i>
+                `)
+                // ponerle clases al elemento
+                divElement.addClass('post__element');
+                divElementData.addClass('post_element__data');
+                divElementicons.addClass('post_element__icons');
+                // añadir dataset
+                divElementData.attr('data-id', e.id);
+                divElementData.attr('data-title', e.title);
+                divElementData.attr('data-body', e.body);
+                // append del elemento
+                divElement.append(divElementData);
+                divElement.append(divElementicons);
+                $posts.append(divElement);
+                // add event listener
+                divElementData.click(onClickData);
+                
+            }
 
         })
-        const printModalPost = ()=>{
-            // peticion al servidor por ID
+    }
+    const printModalPost = (data)=>{
+        const $close = $('.modalClose')
+        $close.click(()=>{
+            $('.modalPost').addClass('hidden')
+        })
+        const printData = (dataFromPetition)=>{
 
-            // print data from petition
+            console.log(dataFromPetition);
+            console.log(data);
+            // //DOM selector
+            $('.modalPost__title').html(data.title)
+            $('.modalPost__body').html(data.body)
+            $('.modalPost__name').html(dataFromPetition.name)
+            $('.modalPost__mail').html(dataFromPetition.email)
+            
+            // // print data from petition
+
         }
+        // peticion al servidor por ID
+        var settings = {
+            "url": `https://jsonplaceholder.typicode.com/users/${data.id}`,
+            "method": "GET",
+            "timeout": 0,
+            "headers": {
+              "tile": "holatio",
+              "Cookie": "__cfduid=de908844017500a5fe40f0af2536d49721610547910"
+            },
+          };
+          
+          $.ajax(settings).done(function (response) {
+            printData(response)
+          });
+
+
+
     }
     const onClickData = (e)=>{
-        console.log(e.target.parentNode.dataset.id);
+        const $modal = $('.modalPost');
+        $modal.removeClass('hidden');
+        printModalPost({
+            id: e.target.parentNode.dataset.id,
+            title: e.target.parentNode.dataset.title,
+            body: e.target.parentNode.dataset.body
+        })
     }
 });
